585b82daf2dbfc1aed3c585ca940b012b561b359
mattglbrt <matt@everyway.io>
2025-01-14 06:03:52 -0500

    world map added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Valtiare',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/mattglbrt/valtiare',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Cosmology',
+					autogenerate: { directory: 'cosmology' },
 				},
 				{
 					label: 'Reference',
--- src/content/docs/reference/example.md
+++ src/content/docs/reference/example.md
@@ -1,5 +1,5 @@
 ---
-title: Example Reference
+title: Welcome
 description: A reference page in my new Starlight docs site.
 ---
 
